8d4bb91a07b7f9ebf78cb8b50823919d0202f681
leongaban <leongaban@gmail.com>
2017-02-28 10:56:04 -0600

    user.services created to handle the http, instead of in the component

---
+++ app/shared/services/user.services.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class UserService {
+
+	// Grab all users
+
+	// Get a single user
+
+	// Create a user
+
+	// Update a user
+
+	// Delete a user
+}
