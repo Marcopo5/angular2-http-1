ed14c17c46acb9c018d764558e432a6be8657553
leongaban <leongaban@gmail.com>
2017-02-28 10:43:57 -0600

    observable working to get data from reqres.in

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,23 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
+import { User } from './shared/models/user';
 
 @Component({
     selector: 'my-app',
     template: `
         <div class="jumbotron text-center">
-        <h1>The App Lives!</h1>
-        <p>{{ message }}</p>
+            <h1>The App Lives!</h1>
+            <p>{{ message }}</p>
+        </div>
+
+        <div *ngIf="users">
+            <div *ngFor="let user of users">
+                <h2>{{ user.first_name }}</h2>
+            </div>
         </div>
     `
 })
 export class AppComponent implements OnInit {
-    
+    users: User[];
     constructor(private http: Http) {}
 
     ngOnInit() {
         this.http.get('https://reqres.in/api/users')
             .subscribe(data => {
-                console.log('data', data)
+                console.log('data', data.json());
+                this.users = data.json().data;
             });
     }
 }
